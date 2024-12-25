f48a4815de9e447dfa83cb534c00b19810f7d0e4
saravana3252 <sara18ec118@gmail.com>
2024-12-26 00:20:10 +0530

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header(){
     return(
         <>
-          <div className="bg-blue-950 w-full h-20 flex justify-between items-center text-white text-2xl px-10 cursor-pointer">
+          <div className="bg-blue-950 w-full h-20 flex justify-between items-center text-white text-2xl lg:px-10 px-2 cursor-pointer">
       <div className="text-lg lg:text-2xl"><Link to="/">Student List</Link></div>
       <div className="text-lg lg:text-2xl"><Link to="/favStudent">Favourite Student List</Link></div>
      </div>
