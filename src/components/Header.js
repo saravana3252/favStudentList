830387fbb8a82503458962986839589c3a3cbbff
saravana3252 <sara18ec118@gmail.com>
2024-12-26 15:55:07 +0530

    ui updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(){
         <>
           <div className="bg-blue-950 w-full h-20 flex justify-between items-center text-white text-2xl lg:px-10 px-4  cursor-pointer">
       <div className="font-bold text-lg lg:text-2xl transition-all duration-300 border border-white p-2 px-4 hover:bg-gray-100 hover:text-blue-950 hover:border-black "><Link to="/">Student List</Link></div>
-      <div className="font-bold text-lg lg:text-2xl "><Link to="/favStudent">Favourite Student List</Link></div>
+      <div className="font-bold text-lg lg:text-2xl "><Link to="/favStudent">Favourites</Link></div>
      </div>
         </>
     )
