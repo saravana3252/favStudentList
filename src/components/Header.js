8305bd387a4c193308c31830cfd06f766d33bba5
saravana3252 <sara18ec118@gmail.com>
2024-12-27 11:49:35 +0530

    header.js updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,15 @@ function Header(props){
         <>
           <div className="bg-blue-950 w-full h-20 flex justify-between items-center text-white text-2xl lg:px-10 px-4  cursor-pointer">
       <div className="font-bold text-lg lg:text-2xl transition-all duration-300 border border-white p-2 lg:px-4 px-2 hover:bg-gray-100 hover:text-blue-950 hover:border-black "><Link to="/">Student List</Link></div>
-      <div className="font-bold text-lg lg:text-2xl flex"><Link to="/favStudent">Favourites</Link><p className=" bg-white rounded-full text-black ml-1.5  px-2">{props.favStudentLenght}</p></div>
+      <div className="bg-white  flex items-center justify-center py-2 rounded-md">
+  <div className="font-bold text-lg lg:text-2xl flex items-center px-2">
+    <Link to="/favStudent" className="text-black ">Favourites</Link>
+    <p className="bg-white rounded-full text-black ml-1.5 px-2">
+      {props.favStudentLenght}
+    </p>
+  </div>
+</div>
+
      </div>
         </>
     )
